e385969555fec72a7335334a2048ab5642f107df
aleshkey <90507705+lesha-oleshkevich@users.noreply.github.com>
2022-10-17 23:46:50 +0300

    Attempt to create smth like inst

---
+++ SpringProgects/inst/src/test/java/com/inst/inst/InstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inst.inst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
